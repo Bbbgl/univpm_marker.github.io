6b77a65cc73b05153ce1e4371909a5a248d48da1
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-02 13:35:25 +0200

    Update distance.js

--- marker-and-gesture/distance.js
+++ marker-and-gesture/distance.js
@@ -18,16 +18,7 @@ window.addEventListener('load', () => {
             console.log(distance);
         }, 100);
     });
-    window.onload = function () {
-      document
-        .querySelector(".say-hi-button")
-        .addEventListener("click", function () {
-          // here you can change also a-scene or a-entity properties, like
-          // changing your 3D model source, size, position and so on
-          // or you can just open links, trigger actions...
-          alert(distance);
-        });
-    };
+   
 
     marker.addEventListener('markerLost', () => {
       clearInterval(check);
