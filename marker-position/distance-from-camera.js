6c829612ea4ce645b70123a83d228d62e7390a3e
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-03 12:36:26 +0200

    Update distance-from-camera.js

--- marker-position/distance-from-camera.js
+++ marker-position/distance-from-camera.js
@@ -1,21 +1,26 @@
+// this script let you get the distance from the marker to the camera, in A-frame
 
 AFRAME.registerComponent('marker-distance', {
-    tick: function () {
+    tick: function () {     //The .tick() handler will get the positions updated every frame
         this.markerDistance()
     },
     markerDistance: function () {
         var marker1Pos, cameraPos
-
+        
+       // see the A-frame documentation : https://aframe.io/docs/master/introduction/javascript-events-dom-apis.html#getting-entities-by-querying-and-traversing
+        
         var marker1 = document.querySelector("#marker1")
         var camera = document.querySelector('[camera]')
-
+        
+        //using Three.js get the marker position value as Three.js Vector3 Object for A-Frame entities
         marker1Pos = new THREE.Vector3();
         marker1.object3D.getWorldPosition(marker1Pos);
-
+        
+        //using Three.js get the camera position value as Three.js Vector3 Object for A-Frame entities
         cameraPos = new THREE.Vector3();
         camera.object3D.getWorldPosition(cameraPos);
 
-        //distance
+        //finding distance using .distanceTo() method.
         distance = marker1Pos.distanceTo(cameraPos);
         console.log("distance" + distance)
         
