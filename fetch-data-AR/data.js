b406ffd8cfc3a33c1c92a7882ffcb5969f5b0d42
Bbbgl <104217088+Bbbgl@users.noreply.github.com>
2022-05-06 16:13:24 +0200

    Update data.js

--- fetch-data-AR/data.js
+++ fetch-data-AR/data.js
@@ -34,7 +34,7 @@ fetch ('https://jsonplaceholder.typicode.com/posts').then(data=>{
     return data.json();
 }).then((completedata)=>{
     //console.log(completedata);
-    data1 = completedata.id;
+    data1 = completedata[2].title;
 
 }).catch((e)=>{
     console.log(e);
